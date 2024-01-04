fdbfd3db6fd0dd42e14ce2e8f222abaac89350e6
natanael santos <https://github.com/NatanaelSantos2/GDScript.git>
2024-01-04 07:27:47 -0300

    Estudos da linguagem reservada para godot

---
+++ GDScript/tipos de varaiveis/variaveis.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var vida:int = 3
+var hp:float = 5.7
+var nome_jogador:String = "Nael"
+var texto_npc:String = "Cuidado aventureiro! tem uma armadilha a frente"
+var lampada:bool = false
+
+
+func _ready():
+	print(vida)
+
+
+
+func _process(delta):
+	pass
