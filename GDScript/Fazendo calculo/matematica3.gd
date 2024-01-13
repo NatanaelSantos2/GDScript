fa124d391b8d07e686a90bafe3cea3d20d981c22
NatanaelSantos2 <154018545+NatanaelSantos2@users.noreply.github.com>
2024-01-13 07:16:26 -0300

    Add files via upload

---
+++ GDScript/Fazendo calculo/matematica3.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+#DECLARAÇÃO DE VARIAVEIS
+var a = 10
+#ADIÇÃO, SUBTRAÇÃO, MULTIPLICAÇÃO E DIVISÃO
+
+func _ready():
+	#IMPRESSÃO DOS VALORES DAS VARIAVEIS
+	print(a)
+	a= 100
+	print(a)
+	a = 1000
+	print(a)
