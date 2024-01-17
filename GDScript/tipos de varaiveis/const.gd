c680cb63a16cefb796c4024cfee5dbc3fd781c56
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-01-16 22:14:48 -0300

    Add files via upload

---
+++ GDScript/tipos de varaiveis/const.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+#DECLARAÇÃO DE VARIAVEIS
+var a
+const b = 100 #Const é usado para definir um valor que você não quer alterar
+const nome = 'Nael'
+const poder = "Fogo"
+
+func _ready():
+	#IMPRESSÃO DOS VALORES DAS VARIAVEIS
+	a = 10
+	print('Variavel -> ',a)
+	print('Constante -> ',b)
+	print('Poder -> ',poder)
