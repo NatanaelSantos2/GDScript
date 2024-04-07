3c2f1f83a871b1ab8382533dd8e3fe11d9e6b891
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-04-07 08:57:01 -0300

    Update caixasProcedural.gd

--- projetosTestes/CaixaAleatorimente/caixasProcedural.gd
+++ projetosTestes/CaixaAleatorimente/caixasProcedural.gd
@@ -10,22 +10,24 @@ var listNumAleatorio:Array = []
 #Linha6 596
 var valorAleatorio:int = 0
 var contTentativas:int = 0
-var caixa1 = 0
-var caixa2 = 0
-var caixa3 = 0
-var caixa4 = 0
-var caixa5 = 0
-var caixa6 = 0
-@onready var caixa_linha_1 = $caixaLinha1
-@onready var caixa_linha_2 = $caixaLinha2
-@onready var caixa_linha_3 = $caixaLinha3
-@onready var caixa_linha_4 = $caixaLinha4
-@onready var caixa_linha_5 = $caixaLinha5
-@onready var caixa_linha_6 = $caixaLinha6
+var caixa1:int  = 0
+var caixa2:int  = 0
+var caixa3:int  = 0
+var caixa4:int  = 0
+var caixa5:int  = 0
+var caixa6:int  = 0
+@onready var caixa_linha_1:Sprite2D = $caixaLinha1
+@onready var caixa_linha_2:Sprite2D = $caixaLinha2
+@onready var caixa_linha_3:Sprite2D = $caixaLinha3
+@onready var caixa_linha_4:Sprite2D = $caixaLinha4
+@onready var caixa_linha_5:Sprite2D = $caixaLinha5
+@onready var caixa_linha_6:Sprite2D = $caixaLinha6
+@onready var portal:Sprite2D = $Portal
 
-func _process(_delta):
-	if contTentativas < 6:
-		valorAleatorio = randi() % 12 + 0
+
+func _process(_delta) -> void:
+	if contTentativas < 7:
+		valorAleatorio = randi() % 13 + 0
 		for a in range(0, list1.size()):
 			if a == valorAleatorio && caixa1 < 1:
 				caixa_linha_1.position.x = list1[a]
@@ -36,7 +38,7 @@ func _process(_delta):
 			caixaAl2()
 		contTentativas += 1
 
-func caixaAl2():
+func caixaAl2() -> void:
 	if listNumAleatorio[0] != valorAleatorio:
 		for c in range(0, list1.size()):
 			if  c == valorAleatorio && caixa2 < 1:
@@ -47,7 +49,7 @@ func caixaAl2():
 		if caixa2 == 1:
 			caixaAl3()
 
-func caixaAl3():
+func caixaAl3() -> void:
 	if listNumAleatorio[0] != valorAleatorio:
 		if listNumAleatorio[1] != valorAleatorio:
 			for f in range(0, list1.size()):
@@ -59,7 +61,7 @@ func caixaAl3():
 			if caixa2 == 1:
 				caixaAl4()
 
-func caixaAl4():
+func caixaAl4() -> void:
 	if listNumAleatorio[0] != valorAleatorio:
 		if listNumAleatorio[1] != valorAleatorio:
 			if listNumAleatorio[2] != valorAleatorio:
@@ -72,7 +74,7 @@ func caixaAl4():
 				if caixa4 == 1:
 					caixaAl5()
 
-func caixaAl5():
+func caixaAl5() -> void:
 	if listNumAleatorio[0] != valorAleatorio:
 		if listNumAleatorio[1] != valorAleatorio:
 			if listNumAleatorio[2] != valorAleatorio:
@@ -86,7 +88,7 @@ func caixaAl5():
 						if caixa5 == 1:
 							caixaAl6()
 
-func caixaAl6():
+func caixaAl6() -> void:
 	if listNumAleatorio[0] != valorAleatorio:
 		if listNumAleatorio[1] != valorAleatorio:
 			if listNumAleatorio[2] != valorAleatorio:
@@ -98,3 +100,12 @@ func caixaAl6():
 								caixa_linha_6.position.y = 596
 								listNumAleatorio.append(valorAleatorio)
 								caixa6 += 1
+						if caixa6 == 1:
+							portalSaida()
+#portal
+func portalSaida() -> void:
+	for l in range(0, list1.size()):
+		if l == valorAleatorio:
+			portal.position.x = list1[l]
+			portal.position.y = 54
+			listNumAleatorio.append(valorAleatorio)
