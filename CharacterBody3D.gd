f0147fe4ceecd8131c0bf86f6c68e417c509b888
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-06-03 11:23:12 -0300

    Add files via upload

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
+	# Pulo.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
